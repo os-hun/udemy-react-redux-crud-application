5b6bf0e3d4db8fdf4849aa743cb166a4614b349b
ShunOmine <shun12baseball@gmail.com>
2019-10-25 10:00:54 +0900

    implement events index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import EventsIndex from './components/events_index';
 import './index.css';
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux"
 import reducer from "./reducers"
+import thunk from "redux-thunk"
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
